22d09c3ed0f70e03463ea22c0bd146f7f217d6e2
apoorv nag <apoorv@rapidinnovation.dev>
2022-11-01 15:54:46 +0530

    Linting in Solidity File

--- contracts/TodoListDemo.sol
+++ contracts/TodoListDemo.sol
@@ -3,55 +3,13 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
-contract Ownable is ReentrancyGuard {
-    // Variable that maintains 
-    // owner address
-    address private _owner;
-
-    // Sets the original owner of 
-    // contract when it is deployed
-    constructor()
-    {
-        _owner = msg.sender;
-    }
-
-    // Publicly exposes who is the
-    // owner of this contract
-    function owner() public view returns(address) 
-    {
-        return _owner;
-    }
-
-    // onlyOwner modifier that validates only 
-    // if caller of function is contract owner, 
-    // otherwise not
-    modifier onlyOwner() 
-    {
-        require(isOwner(),
-        "Function accessible only by the owner !!");
-        _;
-    }
-
-    // function for owners to verify their ownership. 
-    // Returns true for owners otherwise false
-    function isOwner() public view returns(bool) 
-    {
-        return msg.sender == _owner;
-    }
-
-    function transferOwnerShip(address _newOwner) onlyOwner nonReentrant public {
-        _owner = _newOwner;
-    }
-}
-
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TodoList is ReentrancyGuard, Ownable {
-
-    enum TodoStatuses{PENDING, COMPLETE}
-    TodoStatuses constant defaultChoice = TodoStatuses.PENDING;
-
-    uint counter = 1;
+    enum TodoStatuses {
+        PENDING,
+        COMPLETE
+    }
 
     // A todo record
     struct Todo {
@@ -60,34 +18,47 @@ contract TodoList is ReentrancyGuard, Ownable {
         // todo status
         TodoStatuses status;
     }
+    TodoStatuses private constant _DEFAULT_CHOICE = TodoStatuses.PENDING;
+
+    uint private _counter = 1;
 
     Todo[] public listOfTodos;
 
-    function createTask (string memory _task) onlyOwner nonReentrant external {
+    function createTask(string memory _task) external onlyOwner nonReentrant {
         Todo memory todo = Todo(_task, TodoStatuses.PENDING);
         listOfTodos.push(todo);
     }
 
-    function getTaskAtIndex(uint _idx) onlyOwner external view returns (string memory) {
-        return listOfTodos[_idx].text;
-    }
-
-    function getTaskStatusAtIndex(uint _idx) onlyOwner external view returns (TodoStatuses) {
-        return listOfTodos[_idx].status;
-    }
-
-    function totalCount() onlyOwner external view returns (uint) {
-        return listOfTodos.length;
-    }
-
-    function markComplete(uint _id) onlyOwner nonReentrant external  {
+    function markComplete(uint _id) external onlyOwner nonReentrant {
         /**
-        * Mark a task as complete
-        */
+         * Mark a task as complete
+         */
         listOfTodos[_id].status = TodoStatuses.COMPLETE;
     }
 
-    function removeTask(uint index) onlyOwner nonReentrant public {
+    function getTaskAtIndex(uint _idx)
+        external
+        view
+        onlyOwner
+        returns (string memory)
+    {
+        return listOfTodos[_idx].text;
+    }
+
+    function getTaskStatusAtIndex(uint _idx)
+        external
+        view
+        onlyOwner
+        returns (TodoStatuses)
+    {
+        return listOfTodos[_idx].status;
+    }
+
+    function totalCount() external view onlyOwner returns (uint) {
+        return listOfTodos.length;
+    }
+
+    function removeTask(uint index) public onlyOwner nonReentrant {
         require(index <= listOfTodos.length, "Tasks are less");
         listOfTodos[index] = listOfTodos[listOfTodos.length - 1];
         listOfTodos.pop();
