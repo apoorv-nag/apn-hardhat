bf0c61f9ea382ec2b92c1a6bd1977db917737c7b
apoorv nag <apoorv@rapidinnovation.dev>
2022-11-07 09:33:49 +0530

    PYT-38 Deploy script TodoList

--- scripts/todolist-deploy-script.js
+++ scripts/todolist-deploy-script.js
@@ -1,5 +1,3 @@
-const { ethers } = require('hardhat');
-
 async function main() {
   const [deployer] = await ethers.getSigners();
 
@@ -7,12 +5,14 @@ async function main() {
 
   console.log('Account Balance:', await deployer.getBalance().toString());
 
-  const Token = await ethers.getContractFactory('Token');
+  const Token = await ethers.getContractFactory('TodoList');
   const token = await Token.deploy();
+  console.log(await token.totalCount());
 
   console.log('Token address: ', token.address);
 }
 
+console.log('hello');
 main()
   .then(() => process.exit(0))
   .catch((e) => {
